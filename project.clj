9ec712af593e5711765e43bd57fd19fc3d1f3c55
Shriphani Palakodety <shriphanip@gmail.com>
2013-09-09 15:48:18 -0400

    forgot to add xpath lib addition

--- project.clj
+++ project.clj
@@ -1,13 +1,13 @@
 (defproject crawler "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "CMU Discussions Crawler"
+  :url "http://blog.shriphani.com"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[clj-http "0.7.6"]
                  [clj-robots "0.6.0"]
+                 [com.github.kyleburton/clj-xpath "1.4.2"]
                  [enlive "1.1.4"]
                  [net.sourceforge.htmlcleaner/htmlcleaner "2.6"]
                  [org.clojure/clojure "1.5.1"]
                  [org.clojure/tools.cli "0.2.4"]
-                 [org.bovinegenius/exploding-fish "0.3.3"]]
-  :main crawler.gplus.core)
+                 [org.bovinegenius/exploding-fish "0.3.3"]])

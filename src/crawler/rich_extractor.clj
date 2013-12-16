be505582c7b6666feb0ab916f4ce654f994b6332
Shriphani Palakodety <shriphanip@gmail.com>
2013-12-16 00:05:36 -0500

    tokenized

--- src/crawler/rich_extractor.clj
+++ src/crawler/rich_extractor.clj
@@ -2,11 +2,23 @@
   "Implementation of the rich URL extractor"
   (:require [crawler.dom :as dom]
             [crawler.rank :as rank]
+            [crawler.utils :as utils]
+            [clojure.set :as clj-set]
             [org.bovinegenius [exploding-fish :as uri]]))
 
 ;;;; A rich extractor performs an initial score assignment based
 ;;;; on how expressive a URL is. A decision to follow is based upon
 ;;;; some statistics.
+
+(defn featurize
+  [xpath nodes]
+  (map
+   (fn [{_ :node href :href text :text}]
+     (let [url-tokens  (set (utils/tokenize-url href))
+           text-tokens (set (utils/tokenize text))]
+       {:url-tokens url-tokens
+        :text-tokens text-tokens}))
+   nodes))
 
 (defn state-action
   [page-src url]
@@ -25,7 +37,10 @@
                                              (nil? (uri/host (:href a-node)))))
                                        nodes)])
                              xpaths-hrefs-text)))]
-    in-host-xhrefs))
+    (map
+     (fn [[xpath nodes]]
+       (featurize xpath nodes))
+     in-host-xhrefs)))
 
 (defn follow-naive?
   "Decide if an Xpath's links are worth following based on a naive score of richness.

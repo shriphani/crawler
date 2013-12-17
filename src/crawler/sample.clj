6baa260386cf33f7c63a728e3bc6470a745feadf
Shriphani Palakodety <shriphanip@gmail.com>
2013-12-17 17:50:09 -0500

    sampling code resides here

---
+++ src/crawler/sample.clj
@@ -0,0 +1,13 @@
+(ns crawler.sample
+  "Module to sample decision responses.
+   A decision is represented by an XPath
+   A sample picks out some links. Visiting is left
+   to the user")
+
+(defn sample
+  "Given a set of decisions, we produce an instance of
+   each decision. In URLspeak:
+   a decision is an XPath
+   a sample is some links from an XPath"
+  [decisions]
+  '*)

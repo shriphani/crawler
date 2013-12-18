36aba49bc3d9f7713e9b3212c9a5faa4ac5ecbcb
Shriphani Palakodety <shriphanip@gmail.com>
2013-12-18 01:43:09 -0500

    sampling code committed

--- src/crawler/sample.clj
+++ src/crawler/sample.clj
@@ -4,10 +4,23 @@
    A sample picks out some links. Visiting is left
    to the user")
 
+(defn sample-a-decision
+  [links blacklist]
+  (take 20 (filter (fn [a-link] (some blacklist)) links)))
+
 (defn sample
   "Given a set of decisions, we produce an instance of
    each decision. In URLspeak:
    a decision is an XPath
-   a sample is some links from an XPath"
-  [decisions]
-  '*)
+   a sample is some links from an XPath
+
+   We are hard-coding samples to 20
+
+   Args:
+    decisions: {Xpath -> URLs}
+    blacklist: [a set of URLs you don't want to follow]"
+  [decisions blacklist]
+  (map
+   (fn [[a-decision info]]
+     (sample-a-decision (map #(:href %) info) blacklist))
+   decisions))

4bb7a12c55384881f01b31604a46b3b9eceddc75
shriphani <spalakod@cs.cmu.edu>
2014-01-23 20:40:20 -0500

    structure driven crawler its own fn now

--- src/crawler/main.clj
+++ src/crawler/main.clj
@@ -5,10 +5,19 @@
             [crawler.utils :as utils]
             [crawler.structure-driven :as structure-driven]))
 
-(defn crawler-options
+(def crawler-options
   [[nil "--structure-driven" "Use the structure driven crawler"]
    [nil "--start" "Entry point for the structure driven crawler"]
    [nil "--example" "Example leaf page url for the structure driven crawler"]])
+
+(defn structure-driven-crawler
+  [start-url example-body]
+  (let [structure-driven-leaf? (fn [x]
+                                 (structure-driven/leaf?
+                                  x example-body))]
+    (crawl/sample-sitemap-lookahead start-url
+                                    structure-driven-leaf?
+                                    structure-driven/extractor)))
 
 (defn -main
   [& args]
@@ -16,17 +25,10 @@
                     (cli/parse-opts crawler-options)
                     :options)]
     (cond (:structure-driven options)
-          (let [start-url    (-> options :start)
-                example-body (-> options
-                                 :example
-                                 download-with-cookie)
-
-                structure-driven-leaf?     (fn [x]
-                                             (structure-driven/leaf?
-                                              x example-body))]
-            (crawl/sample-sitemap-lookahead start-url
-                                            structure-driven-leaf?
-                                            structure-driven/extractor))
+          (structure-driven-crawler (-> options :start)
+                                    (-> options
+                                        :example
+                                        utils/download-with-cookie))
 
           :else
           (println "Pick one bruh"))))

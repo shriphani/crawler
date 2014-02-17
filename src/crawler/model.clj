01e44bae70886157a008f5259ef8ee47d15e9346
shriphani <spalakod@cs.cmu.edu>
2014-02-17 11:22:19 -0500

    added model

--- src/crawler/model.clj
+++ src/crawler/model.clj
@@ -10,41 +10,10 @@
   (with-open [rdr (io/reader a-model-file)]
     (-> rdr PushbackReader. read)))
 
-(defn xpath-bad-separation-quality
-  [{model :model corpus :corpus}]
-  (let [same-xpath-groups (group-by :src-xpath
-                                    (vals corpus))]
-    (filter
-     #(< 1 (second %))
-     (map
-      (fn [a-group]
-        (let [members (second a-group)]
-          [(first a-group)
-           (count
-            (distinct
-             (map
-              #(->> %
-                    :state-action
-                    :xpath-nav-info
-                    (map :xpath)
-                    set)
-              members)))]))
-      same-xpath-groups))))
-
-(defn handle-xpaths
-  [{model :model corpus :corpus} src-xpath grps]
-  (let [urls (set
-              (map
-               #(-> % second :src-url)
-               (filter
-                #(= (-> % second :src-xpath)
-                    src-xpath)
-                corpus)))
-
-        bodies (map
-                (fn [a-url] [a-url (-> a-url corpus :body)])
-                urls)]
-    (map
-     (fn [[url body]]
-       (dom/refine-xpath body url (first src-xpath) grps))
-     bodies)))
+(defn plan
+  "Plan of execution for the model"
+  [a-model]
+  (let [total (apply + (map second a-model))]
+    (sort-by (juxt
+              #(-> % first count)
+              #(- total (second %))) a-model)))

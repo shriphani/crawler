4181de208cf99f79c1c950b6d9822792da99da77
shriphani <spalakod@cs.cmu.edu>
2014-03-27 13:43:24 -0400

    added corpus docs

--- src/crawler/corpus.clj
+++ src/crawler/corpus.clj
@@ -1,10 +1,13 @@
 (ns crawler.corpus
+  "Tools for processing a downloaded corpus"
   (:require [cheshire.core :as json]
             [clojure.java.io :as io])
   (:use [clojure.pprint :only [pprint]])
   (:import [java.io PushbackReader]))
 
 (defn corpus->json
+  "Converts a corpus file which is essentially a pprinted map
+   to a json file that python or someone else can handle"
   [a-corpus-file]
   (let [rdr (PushbackReader.
              (io/reader a-corpus-file))

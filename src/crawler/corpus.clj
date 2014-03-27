d3619f5bc5b25932bd7d8fd4b737e4d1c63ef301
shriphani <spalakod@cs.cmu.edu>
2014-03-27 13:46:21 -0400

    corpus reading is its own routine

--- src/crawler/corpus.clj
+++ src/crawler/corpus.clj
@@ -5,14 +5,18 @@
   (:use [clojure.pprint :only [pprint]])
   (:import [java.io PushbackReader]))
 
+(defn read-corpus-file
+  [a-corpus-file]
+  (let [rdr (PushbackReader.
+             (io/reader a-corpus-file))]
+    (read rdr)))
+
 (defn corpus->json
   "Converts a corpus file which is essentially a pprinted map
    to a json file that python or someone else can handle"
   [a-corpus-file]
-  (let [rdr (PushbackReader.
-             (io/reader a-corpus-file))
-        wrtr (io/writer
+  (let [wrtr (io/writer
               (clojure.string/replace a-corpus-file #".corpus" ".json"))]
      (json/generate-stream
-      (read rdr)
+      (read-corpus-file a-corpus-file)
       wrtr)))

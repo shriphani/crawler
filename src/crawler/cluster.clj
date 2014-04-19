84bf4afe3eaddc754fbb9ea7a143772a11976c6e
shriphani <spalakod@cs.cmu.edu>
2014-04-19 18:19:06 -0400

    added clustering module

--- src/crawler/cluster.clj
+++ src/crawler/cluster.clj
@@ -1,21 +1,57 @@
 (ns crawler.cluster
   "Clustering utilities for explorations by the crawler")
 
-(defn assign-where?
-  "At least 1 cluster member satisfies the membership test"
-  [clusters x membership-test]
-  (.indexOf
-   (map
-    (fn [a-cluster]
-      (membership-test a-cluster x))
-    clusters)
-   true))
+(defn merge?
+  [cluster1 cluster2 similar?]
+  (some
+   identity
+   (flatten
+    (map
+     (fn [x]
+       (map
+        (fn [y]
+          (if (similar? x y)
+            true))
+        cluster2))
+     cluster1))))
 
 (defn assign
-  "Returns a new set of clusters
-   after adding object x to them"
-  [clusters x membership-test]
-  (let [position (assign-where? clusters x membership-test)]
-    (if (<= 0 position)
-      (assoc clusters position (cons x (nth clusters position)))
-      (cons [x] clusters))))
+  [clusters x similar?]
+  (let [is
+        (filter
+         identity
+         (map
+          (fn [[i c]]
+            (if (merge? c x similar?)
+              i
+              nil))
+          (map vector
+               (range (count clusters))
+               clusters)))]
+    (if (empty? is)
+      -1 (first is))))
+
+(defn cluster
+  "Points: a set of points
+   similar? : are x and y similar?"
+  [points similar?]
+  (cond (-> points first vector?)
+        (let [merged (set
+                      (reduce
+                       (fn [acc x]
+                         (let [assign-to (assign acc x similar?)]
+                           (if (neg? assign-to)
+                             (into [] (cons x acc))
+                             (assoc (into [] acc) assign-to (into
+                                                             []
+                                                             (concat (nth acc assign-to)
+                                                                     x))))))
+                       []
+                       points))]
+          (if (= merged points)
+            merged
+            (recur merged similar?)))
+
+        :else
+        (recur (set (map (fn [x] [x]) points))
+               similar?)))
